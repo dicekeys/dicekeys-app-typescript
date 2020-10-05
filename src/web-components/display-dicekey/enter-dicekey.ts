28a22b4d835507e71b24703c02ad84a2e1c23dd4
Stuart Schechter <stuart.schechter@gmail.com>
2020-10-06 08:25:48 +0900

    add more keyboard nav

--- src/web-components/display-dicekey/enter-dicekey.ts
+++ src/web-components/display-dicekey/enter-dicekey.ts
@@ -89,27 +89,58 @@ export class EnterDiceKey extends Component<EnterDiceKeyOptions, SVGSVGElement> 
         this.currentFaceIndex = this.nextFaceIndex;
       }
       this.currentFace.digit = upperKey;
-    } else if (event.code === "Space") {
+    } else if (
+      ((event.shiftKey || event.ctrlKey) && event.code === "ArrowRight") ||
+      event.key === "+" ||
+      event.key === "=" ||
+      event.key === "]" ||
+      event.key === "." ||
+      event.key === ">" ||
+      event.key === "'"
+    ) {
+      // Rotate right
       this.currentFace.orientationAsLowercaseLetterTrbl =
-        FaceOrientationLetterTrblOrUnknown.rotate(this.currentFace.orientationAsLowercaseLetterTrbl ?? 't', 1) ?? 't';
+        FaceOrientationLetterTrblOrUnknown.rotate(this.currentFace.orientationAsLowercaseLetterTrbl ?? 't', 1) ?? 't';      
+    } else if (
+      ( (event.shiftKey || event.ctrlKey) && event.code === "ArrowLeft") ||
+      event.key === "_" ||
+      event.key === "-" ||
+      event.key === "[" ||
+      event.key === "," ||
+      event.key === "<" ||
+      event.key === ";"
+    ) {
+      // Rotate left
+      this.currentFace.orientationAsLowercaseLetterTrbl =
+        FaceOrientationLetterTrblOrUnknown.rotate(this.currentFace.orientationAsLowercaseLetterTrbl ?? 't', 3) ?? 't';
     } else if (event.code === "ArrowUp") {
+      // Move up ( index -= 5 % 25 )
       this.currentFaceIndex = ( this.currentFaceIndex + 20 ) % 25;
     } else if (event.code === "ArrowDown") {
+      // Move down ( index += 5 % 25 )
       this.currentFaceIndex = ( this.currentFaceIndex + 5 ) % 25;
-    } else if (event.code === "ArrowLeft" || (event.code === "Tab" && event.shiftKey === true)) {
+    } else if (event.code === "ArrowLeft" || ((event.shiftKey || event.ctrlKey) && (event.code === "Tab" || event.code === "Space"))) {
+      // Move left
       this.currentFaceIndex = this.previousFaceIndex;
-    } else if (event.code === "ArrowRight" || (event.code === "Tab" && event.shiftKey === false)) {
+    } else if (event.code === "ArrowRight" || ((!event.shiftKey && !event.ctrlKey) && (event.code === "Tab" || event.code === "Space"))) {
+      // Move right
       this.currentFaceIndex = this.nextFaceIndex;
     } else if (event.code === "Delete") {
       delete this.currentFace.letter;
       delete this.currentFace.digit;
+      this.currentFace.orientationAsLowercaseLetterTrbl = 't';
     } else if (event.code === "Backspace") {
       if (this.currentFace.letter == null && this.currentFace.digit == null) {
         this.currentFaceIndex = this.previousFaceIndex;
       } else {
         delete this.currentFace.letter;
         delete this.currentFace.digit;
+        this.currentFace.orientationAsLowercaseLetterTrbl = 't';
       }
+    } else if (event.code === "Home") {
+      this.currentFaceIndex = 0;
+    } else if (event.code === "End") {
+      this.currentFaceIndex = 24;
     }
     event.preventDefault();
     this.renderSoon();
