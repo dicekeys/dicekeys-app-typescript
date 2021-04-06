a485d77db313a6b33f563244de353a4c50c97f16
Stuart Schechter <stuart@dicekeys.com>
2021-04-06 20:48:49 +0900

    more cleanup

--- src/state/dicekey-state.ts
+++ src/state/dicekey-state.ts
@@ -1,62 +1,40 @@
-import {
-  AppStateStore,
-} from "../web-component-framework/locally-stored-state";
-import {
-  SeededCryptoModuleWithHelpers,
-} from "@dicekeys/seeded-crypto-js";
+// import {
+//   SeededCryptoModuleWithHelpers,
+// } from "@dicekeys/seeded-crypto-js";
 // import {
 //   diceKeyIdToNicknameMap
 // } from "./known-dicekeys"
-import {
-  PublicKeySet,
-  PopulatePublicKeyCacheFn, populatePublicKeyCacheFnFactory
-} from "./public-key-set";
+// import {
+//   PublicKeySet,
+//   PopulatePublicKeyCacheFn, populatePublicKeyCacheFnFactory
+// } from "./public-key-set";
+import { autoSave } from "./core/auto-save";
+import { makeAutoObservable } from "mobx";
 
-const rewriteFieldName = (keyId: string) => (fieldName: string) => `DiceKeyState:${keyId}:${fieldName}`;
-
-export class DiceKeyStateStore extends AppStateStore {
-  public readonly populatePublicKeyCache: PopulatePublicKeyCacheFn;
+export class DiceKeyStateStore {
 
   constructor(
-    protected readonly seededCryptoModule: SeededCryptoModuleWithHelpers,
     protected readonly keyId: string
   ) {
-    super(rewriteFieldName(keyId));
-    this.populatePublicKeyCache = populatePublicKeyCacheFnFactory(this.seededCryptoModule,  this);
+    makeAutoObservable(this);
+    autoSave(this, `DiceKeyStateStore:${keyId}`)
   }
 
-  public centerDieHumanReadableForm = this.addStringField("centerDie");
-//  public readonly nicknameField = this.addStringField("nickname");
-  public readonly desiredPublicKeyCacheSize = this.addField<number>("desiredPublicKeyCacheSize");
-  // public get nickname() { return this.nicknameField.value }
-  // public set nickname(newNickname: string | undefined) {
-  //   this.nicknameField.set(newNickname);
-  //   const newMap = diceKeyIdToNicknameMap.value ?? {};
-  //   if (newNickname != null) {
-  //     newMap[this.keyId] = newNickname
-  //   } else {
-  //     delete newMap[this.keyId];
-  //   }
-  //   diceKeyIdToNicknameMap.set(newMap);
-  // }
-  public readonly publicKeySetField = this.addField<PublicKeySet>("publicKeySet");
-  public get publicKeySet() { return this.publicKeySetField?.value }
-  public set publicKeySet(value: PublicKeySet | undefined) { this.publicKeySetField?.set(value) }
-  public readonly hasBeenReadWithoutError = this.addField<boolean>(`hasBeenReadWithoutError`);
-  public readonly hasBeenBackedUpToWords = this.addField<boolean>(`hasBeenBackedUpToWords`);
-  public readonly hasBeenBackedUpToReplica = this.addField<boolean>(`hasBeenBackedUpToReplica`);
-  public readonly dontAskAboutBackupAgain = this.addField<boolean>("dontAskAboutBackupAgain");
+  centerDieHumanReadableForm?: string;
+  hasBeenReadWithoutError: boolean = false;
+  hasBeenBackedUpToWords: boolean = false;
+  hasBeenBackedUpToReplica: boolean = false;
+  dontAskAboutBackupAgain: boolean = false;
   
-  public get hasSealingKey() { return (this.publicKeySet?.sealingKeys.length ?? 0) > 0 }
-  public get hasSignatureVerificationKey() { return (this.publicKeySet?.signatureVerificationKeys.length ?? 0) > 0 }
+  // get hasSealingKey() { return (this.publicKeySet?.sealingKeys.length ?? 0) > 0 }
+  // get hasSignatureVerificationKey() { return (this.publicKeySet?.signatureVerificationKeys.length ?? 0) > 0 }
 
   private static instances = new Map<string, DiceKeyStateStore>();
-  public static instanceFor = (
-    seededCryptoModule: SeededCryptoModuleWithHelpers,
+  static instanceFor = (
     keyId: string
   ) => {
     if (!DiceKeyStateStore.instances.has(keyId)) {
-      DiceKeyStateStore.instances.set(keyId, new DiceKeyStateStore(seededCryptoModule, keyId))
+      DiceKeyStateStore.instances.set(keyId, new DiceKeyStateStore(keyId))
     }
     return DiceKeyStateStore.instances.get(keyId)!;
   }
