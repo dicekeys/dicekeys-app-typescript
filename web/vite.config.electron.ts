41c64d45468bea01aeb40602b8e2b47f373ca371
Stuart Schechter <stuart@dicekeys.com>
2021-10-15 17:52:07 -0400

    fix electron build... use relative paths in electron build use old commonjs for electron compile elctron into a dist directory import from the dist directory

--- web/vite.config.electron.ts
+++ web/vite.config.electron.ts
@@ -6,13 +6,16 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   root: "./src/",
+  // This option forces the generation of relative paths, which electron needs. 
+  base: "./",
   build: {
     // Do not minify for Electron
     minify: false,
     // Required for dependency on BigInt and number literals ending in n (1n)
     target: "es2020",
     // Write output into electron subdirectory of repository's /dist directory
-    outDir: "../../dist/electron/",
+    outDir: "../../dist/electron-html/",
+    // We love source maps for debugging, and since we're open source, there's no reason to hide 'em.
     sourcemap: true,
     
     // Compile the electron.html file as the root
