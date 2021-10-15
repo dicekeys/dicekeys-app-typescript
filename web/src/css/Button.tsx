087cfc666d023e7d14a7eb6ab149d31d0c841896
Stuart Schechter <stuart@dicekeys.com>
2021-10-15 08:26:14 -0400

    use common ancestor for button styles

--- web/src/css/Button.tsx
+++ web/src/css/Button.tsx
@@ -1,23 +1,5 @@
 import styled from "styled-components";
 
-export const PushButton = styled.button<{invisible?: boolean}>`
-  display: block;
-  cursor: grab;
-  font-size: 1rem;
-  user-select: none;
-  visibility: ${props => props.invisible ? "hidden" : "visible"};
-  width: fit-content;
-  align-self: center;
-  border-radius: 0.25rem;
-  padding: 0.25rem;
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  margin: 0.5rem;
-  &:hover {
-    background: rgba(128,128,128,0.3);
-  }
-`;
-
 export const ButtonRow = styled.div`
   width: 100%;
   display: flex;
@@ -27,31 +9,46 @@ export const ButtonRow = styled.div`
   align-items: baseline;
 `;
 
+export const BaseButton = styled.button<{invisible?: boolean}>`
+  display: block;
+  visibility: ${props => props.invisible ? "hidden" : "visible"};
+  cursor: grab;
+  font-size: 1rem;
+  user-select: none;
+  width: fit-content;
+  align-self: center;
+  &:hover {
+    background: rgba(128,128,128,0.3);
+  }
+`;
+
+export const PushButton = styled(BaseButton)`
+  border-radius: 0.25rem;
+  padding: 0.25rem;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+  margin: 0.5rem;
+`;
+
 export const OptionButton = styled(PushButton)`
   &:not(:first-child) {
     margin-left: 2rem;
   }
 `
 
-export const StepButton = styled.button<{invisible?: boolean}>`
+export const StepButton = styled(BaseButton)`
   min-width: 3rem;
-  cursor: grab;
-  justify-content: center;
-  align-items: baseline;
+  text-align: center;
   background: none;
+  outline: 0;
   border: none;
   margin-left: 0.25rem;
   margin-right: 0.25rem;
   border-radius: 0.25rem;
-  user-select: none;
-  visibility: ${props=>props.invisible ? "hidden":"visible"};
-  &:hover {
-    background-color: rgba(128,128,128,1);
-  }
+  padding: 0.25rem;
   &:focus {
     outline:0;
   }
   &:active {
     background-color: gray
-  }
-`;
+  }`;
