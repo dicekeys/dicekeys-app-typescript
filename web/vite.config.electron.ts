2f1a81995552288182b4154c6053a1a1526e0575
Stuart Schechter <stuart@dicekeys.com>
2021-10-15 16:17:13 -0400

    have vite build electron.html into /dist and not /dist/src/

--- web/vite.config.electron.ts
+++ web/vite.config.electron.ts
@@ -5,14 +5,15 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-//  root: "./src/",
+  root: "./src/",
   build: {
     // Do not minify for Electron
     minify: false,
     // Required for dependency on BigInt and number literals ending in n (1n)
     target: "es2020",
     // Write output into electron subdirectory of repository's /dist directory
-    outDir: "../dist/electron/",
+    outDir: "../../dist/electron/",
+    
     // Compile the electron.html file as the root
     rollupOptions: {
       input: {
