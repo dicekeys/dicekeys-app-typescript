2003bc13d7ade9f26f4c0ff34f4ec57cb88ecafa
Stuart Schechter <stuart@dicekeys.com>
2021-11-23 10:23:09 -0500

    add hamburger menu make home page more responsive to resize

--- web/src/views/Navigation/TopNavigationBar.tsx
+++ web/src/views/Navigation/TopNavigationBar.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
+import { cssCalcTyped } from "../../utilities";
 
 export const TopNavigationBarHeightInVh = 7;
+
+const topNavFontSize = cssCalcTyped(`min(${`3.75vh`},${`8.5vw`})`);
+
 
 // export const NavBarBackgroundColor = "#5576C5";
 
@@ -10,7 +14,7 @@ export const NavigationBar = styled.div`
   flex-direction: row;
   flex: 0 0 auto;
   text-align: center;
-  font-size: 3.75vh;
+  font-size: ${topNavFontSize};
   overflow: hidden;
 `;
 
@@ -36,12 +40,11 @@ export const BelowTopNavigationBarWithSideMarginsButNoBottomBar = styled(BelowTo
   justify-content: space-around;
 `
 
-
 const TopNavRegion = styled.div`
   margin-top: 0.5rem;
   margin-bottom: 0.5rem;
   user-select: none;
-  font-size: 3.75vh;
+  font-size: ${topNavFontSize};
   overflow: hidden;
 `;
 
@@ -49,10 +52,7 @@ const TopEdgeNavRegion = styled(TopNavRegion)`
   display: flex;
   flex-direction: row;
   align-items: center;
-  min-width: 10%;
-  max-width: 12.5%;
-  flex-basis: 12.5%;
-  flex-shrink: 1;
+  width: 12.5vw;
   padding-left: 1rem;
   padding-right: 1rem;
 `;
@@ -63,9 +63,7 @@ export const TopNavLeftSide = styled(TopEdgeNavRegion)`
 `;
 
 export const TopNavCenter = styled(TopNavRegion)`
-  flex-basis: 70%;
-  flex-grow: 2;
-  flex-shrink: 0;
+  width: 85vw;
 `;
 
 export const TopNavRightSide = styled(TopEdgeNavRegion)`
@@ -80,8 +78,8 @@ export const TopNavPopUpMenu = styled.div<{isOpen: boolean}>`
   align-items: flex-start;
   min-width: 10%;
   width: fit-content;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  /* padding-left: 1rem;
+  padding-right: 1rem; */
   background-color: ${ props => props.theme.colors.navigationBar };
   ${ ({isOpen}) => isOpen ? `` : `visibility: hidden` }
 `;
@@ -89,7 +87,7 @@ export const TopNavPopUpMenu = styled.div<{isOpen: boolean}>`
 export const Clickable = styled.span`
   cursor: grab;
   :hover {
-    color: ${ props => props.theme.colors.background} // invert(${ (props) => props.theme.colors.foreground });
+    color: ${ props => props.theme.colors.background}
   }
 `;
 
--- web/src/views/WindowHomeNavigationBar.tsx
+++ web/src/views/WindowHomeNavigationBar.tsx
@@ -7,25 +7,59 @@ import {
 import { WindowTopLevelNavigationState } from "../state/Window";
 import { BUILD_VERSION, BUILD_DATE } from "../vite-build-constants";
 import { BooleanState } from "../state/reusable/BooleanState";
+import styled from "styled-components";
+import { DiceKeyWithKeyId } from "../dicekeys/DiceKey";
 
 // &#8801; is hamburger menu?
 
 const ExpandedMenu = observer ( ({children, booleanStateTrueIfMenuExpanded, ...props}: PropsWithChildren<{booleanStateTrueIfMenuExpanded: BooleanState} >) => (
-  <TopNavRightPopUpMenu isOpen={booleanStateTrueIfMenuExpanded.value} {...props}>Yo!</TopNavRightPopUpMenu>
+  <TopNavRightPopUpMenu isOpen={booleanStateTrueIfMenuExpanded.value} {...props}>{children}</TopNavRightPopUpMenu>
 ))
- 
-const WindowHomeMenu = observer ( (props: {booleanStateTrueIfMenuExpanded: BooleanState}) => (
-  <ExpandedMenu {...props}>Yo!</ExpandedMenu>
+
+const MenuItem = styled.div`
+  align-self: stretch;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: baseline;
+  border-top-color: ${ props => props.theme.colors.navigationBarForeground };
+  border-top-style: dotted;
+  border-top-width: 1px;
+  padding-top: 0.25rem;
+  padding-bottom: 0.25rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-size: 1rem;
+  user-select: none;
+  :hover {
+    color: ${ props => (props.onClick != null) ? props.theme.colors.background : props.theme.colors.navigationBarForeground }
+  }
+`;
+
+const MenuItemDisplayOnlyContent = styled.div`
+  font-style: italic;
+  text-align: left;
+  font-size: 0.8rem;
+`
+
+const WindowHomeMenu = observer ( ({state, ...props}: {state: WindowTopLevelNavigationState, booleanStateTrueIfMenuExpanded: BooleanState}) => (
+  <ExpandedMenu {...props}>
+    <MenuItem onClick={async () => {
+      state.navigateToSelectedDiceKeyView(await DiceKeyWithKeyId.fromRandom());
+    }}>Load random DiceKey</MenuItem>
+    {/* <MenuItem>Yo!</MenuItem> */}
+    <MenuItem><MenuItemDisplayOnlyContent>Version {BUILD_VERSION}<br/>{BUILD_DATE}</MenuItemDisplayOnlyContent></MenuItem>
+  </ExpandedMenu>
 ) );
 
 export const WindowHomeNavigationBar = observer( ( {
-//  state,
+  state,
 //   goBack
 }: {state: WindowTopLevelNavigationState}) => {
   const booleanStateTrueIfMenuExpanded = new BooleanState();
   return (
     <TopNavigationBar>
-      <TopNavLeftSide style={{fontSize: `min(1.25vh, 1.25vw)`, alignSelf: `flex-end`}}>v{BUILD_VERSION}, {BUILD_DATE}</TopNavLeftSide>
+      <TopNavLeftSide style={{fontSize: `min(1.25vh, 1.25vw)`, alignSelf: `flex-end`}}></TopNavLeftSide>
       <TopNavCenter>DiceKeys App</TopNavCenter>
       <TopNavRightSide>
         <Clickable
@@ -35,7 +69,7 @@ export const WindowHomeNavigationBar = observer( ( {
           &#8801;
         </Clickable>
       </TopNavRightSide>
-      <WindowHomeMenu {...{booleanStateTrueIfMenuExpanded}} />
+      <WindowHomeMenu {...{state, booleanStateTrueIfMenuExpanded}} />
     </TopNavigationBar>
   )
 });
