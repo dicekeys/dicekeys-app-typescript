d2453c53f922539fcf3246aae6e571540dc50635
Stuart Schechter <stuart@dicekeys.com>
2021-11-26 17:52:21 -0500

    fix build constants

--- web/src/vite-build-constants.ts
+++ web/src/vite-build-constants.ts
@@ -2,5 +2,8 @@
 export const RUNNING_IN_ELECTRON = !! VITE_SET_APP_RUNNING_IN_ELECTRON;
 export type RUNNING_IN_ELECTRON = typeof RUNNING_IN_ELECTRON;
 
-export const BUILD_DATE = `VITE_BUILD_DATE`;
-export const BUILD_VERSION = `VITE_BUILD_VERSION`;
+declare var VITE_BUILD_DATE: string;
+declare var VITE_BUILD_VERSION: string;
+
+export const BUILD_DATE = VITE_BUILD_DATE;
+export const BUILD_VERSION = VITE_BUILD_VERSION;
